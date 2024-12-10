80dc8422453f23b75f2dc5b708085f34b730f2e1
ypavanr <ypavanreddy190@gmail.com>
2024-12-11 00:17:38 +0530

    env file

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,53 +2,41 @@ import express from "express";
 import bodyParser from "body-parser";
 import pg from "pg";
 import cors from "cors";
+import env from "dotenv";
 const app=express()
+env.config()
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin:'http://localhost:5173'
+    origin:process.env.CORS_ORIGIN_URL
 }));
 app.listen(3000,()=>{
     console.log("server listening on port 3000")
 })
 const db = new pg.Client({
-    user: "postgres",
-    host: "localhost",
-    database: "bank",
-    password:"pgsql",
-    port: 5432,
+    user: process.env.PG_USER,
+    host: process.env.PG_HOST,
+    database: process.env.PG_DATABASE,
+    password:process.env.PG_PASSWORD,
+    port: process.env.PG_PORT,
   });
   db.connect().then(()=>{
     console.log("connected to database")
   })
 
   app.post('/register', (req, res) => {
-    // Collect the data from the request body
-    const { name, email, password, phone, birthdate } = req.body;
-
-    // Log the received data (for testing purposes)
-    console.log('Received registration data:', { name, email, password, phone, birthdate });
-
-    // Perform further processing (e.g., validate inputs, store data in a database)
+    const { name,accountNo, email, password, phone, birthdate,accountType } = req.body;
+    console.log('Received registration data:', { name,accountNo, email, password, phone, birthdate,accountType });
     if (!name || !email || !password || !phone || !birthdate) {
         return res.status(400).json({ error: 'All fields are required!' });
     }
-
-    // Simulate successful data storage
     res.status(200).json({ message: 'User registered successfully!' });
 });
 app.post('/login', (req, res) => {
-    // Collect the data from the request body
     const { email, password } = req.body;
-
-    // Log the received data (for testing purposes)
     console.log('Received login data:', { email, password});
-
-    // Perform further processing (e.g., validate inputs, store data in a database)
     if ( !email || !password ) {
         return res.status(400).json({ error: 'All fields are required!' });
     }
-
-    // Simulate successful data storage
     res.status(200).json({ message: 'User login successfully!' });
 });
